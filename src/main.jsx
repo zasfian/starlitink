5033831d935679176f58318ae9b4c0ec4572c637
zasfian <zasfiandreams@gmail.com>
2026-01-20 13:53:00 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FlipbookPDF from "./App";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FlipbookPDF />
+    <App />
   </React.StrictMode>
 );
