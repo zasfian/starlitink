6277ffc2d7ded69220d7c61802c2b5120b8200ee
zasfian <zasfiandreams@gmail.com>
2026-01-20 13:40:34 +0800

    Update ReaderPage.jsx

--- src/ReaderPage.jsx
+++ src/ReaderPage.jsx
@@ -7,7 +7,7 @@ export default function ReaderPage() {
 
       <section className="reader-frame">
         <iframe
-          src="PASTE_YOUR_FLIPHTML5_URL_HERE"
+          src="<div style="position:relative;padding-top:max(60%,324px);width:100%;height:0;"><iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/bofup/khju/" title="DREAMERS" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe></div>"
           title="Starlit Ink Reader"
           allowFullScreen
         />
