ee319ecdad63fffcff1b76c01a509fe8ab6b0786
zasfian <zasfiandreams@gmail.com>
2026-01-20 13:27:16 +0800

    Create ReaderPage.jsx

---
+++ src/ReaderPage.jsx
@@ -0,0 +1,14 @@
+export default function ReaderPage() {
+  return (
+    <div>
+      <h1>Starlit Ink</h1>
+
+      <iframe
+        src="<div style="position:relative;padding-top:max(60%,324px);width:100%;height:0;"><iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/bofup/khju/" title="DREAMERS" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe></div>"
+        width="100%"
+        height="600"
+        title="Starlit Ink Reader"
+      />
+    </div>
+  );
+}
